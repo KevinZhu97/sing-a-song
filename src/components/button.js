19667d9d6a7d3a608d8e9aae45393ff284ce31dd
Kevin Zhu <kevinzhu@Kevins-Air.cpe.cableonda.net>
2022-09-27 10:44:07 -0500

    Testing

--- src/components/button.js
+++ src/components/button.js
@@ -1,6 +1,7 @@
 
 
 const Button = (props) => {
+    console.log("Button clicked")
     return (
         <button onClick={props.onClick} type="button" className="authenticate-button">{props.purpose}</button>
     )
